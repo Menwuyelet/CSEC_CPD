74eb02d14c890571d936839b60254728b86ba5f4
Menu Temesgen <menutemesgen@gmail.com>
2025-04-05 18:10:00 +0300

    - updated some problem solutions to optimize. got problems AllDivisionsWiththeHighestScoreofaBinaryArray, findTehWinnerOfTheCircularGame2 and imageSmoother i optimized their solutions.

--- Progrsess/imageSmoother.py
+++ Progrsess/imageSmoother.py
@@ -6,20 +6,16 @@ ans = [[0]*m for _ in range(n)]
 
 for i in range(n):
     for j in range(m):
-        idx = [i,j]
         min_row = max(i-1, 0)
         min_column = max(j-1, 0)
         max_row = min(i+1, n-1)
         max_column = min(j+1, m-1)
-
-        sum = 0
-        len = 0
-        for row in range(min_row, max_row + 1):
-            for column in range(min_column, max_column+1):
-                sum+=img[row][column]
-                len +=1
-        ans[i][j] = sum//len
-
+        sum, count = 0, 0
+        for r in range(min_row, max_row+1):
+            for c in range(min_column, max_column+1):
+                sum += img[r][c]
+                count +=1
+        ans[i][j] = sum//count
 for i in ans:
     print(i)
 
---
+++ aprils_fool/...AndAfterHappliyLivedEverThey.py
@@ -0,0 +1,21 @@
+def countSwaps(a,n):
+    move = True
+    count = 0
+    while move:
+        move = False
+        for i in range(n-1):
+            if a[i]>a[i+1]:
+                a[i], a[i+1] = a[i+1], a[i]
+                count += 1 
+                move = True
+    # Write your code here
+    print(f"Array is sorted in {count} swaps.")
+    print (f"First Element: {a[0]}" )
+    print (f"Last Element: {a[-1]}")
+
+n = int(input())
+a = list(map(int, input().rstrip().split()))
+
+
+
+countSwaps(a, n)
