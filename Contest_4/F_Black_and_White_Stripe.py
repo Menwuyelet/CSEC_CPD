c3e613bf2cdc637e5487f923058cfe8e00f7b295
Menu Temesgen <menutemesgen@gmail.com>
2025-04-10 09:20:15 +0300

    - I solved the problem F from the Contest_4 after the contest.

--- Contest_4/F_Black_and_White_Stripe.py
+++ Contest_4/F_Black_and_White_Stripe.py
@@ -3,32 +3,13 @@ for _ in range(t):
     n, k = map(int, input().split())
     s = input()
 
-    count = 0
-    i = 0
-    if n == k:
-        while i < n:
-            temp = 0
-            j = i
-            if s[i] == "B":
-                while j < n:
-                    if s[j] == "B":
-                        temp+=1
-                    j+=1
-            i+=1
-            count = max(count, temp)
-    else:
-        while i < n-k:
-            temp = 0
-            j = i
-            if s[i] == "B":
-                while j < i+k:
-                    if s[j] == "B":
-                        temp+=1
-                    j+=1
-            i+=1
-            count = max(count, temp)
+    count_white = s[:k].count("W")
+    min_white = count_white
 
-    if k < count:
-        print(0)
-    else:
-        print(k-count)
+    for i in range(k, n):
+        if s[i-k] == "W":
+            count_white -= 1
+        if s[i] == "W":
+            count_white += 1
+        min_white = min(min_white, count_white)
+    print(min_white)
